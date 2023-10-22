c021704d30b2c8854e43f012153fa6b5b7c1fe0e
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-10-22 11:32:29 -0400

    Redirecting and wildcard routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
@@ -40,8 +41,19 @@ const appRoutes: Routes = [
         path: ':id/edit',
         component: EditServerComponent
       }
-    ]
+    ],
   },
+  /*
+    Handling error routes should be at the end of other routes.
+   */
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
 
 ];
 
@@ -53,7 +65,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
